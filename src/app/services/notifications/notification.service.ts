a16683acf1f9107b15a5a7503b9d1b4d93173d47
Pablo Valenzuela <jopavalenzuelami@ittepic.edu.mx>
2020-02-12 23:43:03 -0700

    feat: Register form updated
    The previous design was not as good as spected, therefore, I had
    to perform some changes.

---
+++ src/app/services/notifications/notification.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { MatSnackBar, MatSnackBarConfig } from '@angular/material';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class NotificationService {
+
+  constructor(public snackBar: MatSnackBar) { }
+
+  config: MatSnackBarConfig = {
+    duration: 3000,
+    horizontalPosition: 'right',
+    verticalPosition: 'bottom'
+  }
+
+
+  success(msg) {
+    this.config['panelClass'] = ['notification', 'success'];
+    this.snackBar.open(msg, '',this.config);
+  }
+
+  warn(msg) {
+    this.config['panelClass'] = ['notification', 'warn'];
+    this.snackBar.open(msg, '', this.config);
+  }
+
+}
